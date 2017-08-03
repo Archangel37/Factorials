1420c7486d1329327a3b21d854271c5f25f949b5
Archangel <djarchangel3737@gmail.com>
2017-08-03 16:57:33 +0300

    starting project, some working code =)

---
+++ Factorials/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Factorials
+{
+    partial class FormFact
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxNum = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxResult = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBoxNum
+            // 
+            this.richTextBoxNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBoxNum.Location = new System.Drawing.Point(168, 48);
+            this.richTextBoxNum.Name = "richTextBoxNum";
+            this.richTextBoxNum.Size = new System.Drawing.Size(537, 33);
+            this.richTextBoxNum.TabIndex = 0;
+            this.richTextBoxNum.Text = "";
+            // 
+            // richTextBoxResult
+            // 
+            this.richTextBoxResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBoxResult.Location = new System.Drawing.Point(12, 164);
+            this.richTextBoxResult.Name = "richTextBoxResult";
+            this.richTextBoxResult.Size = new System.Drawing.Size(758, 431);
+            this.richTextBoxResult.TabIndex = 1;
+            this.richTextBoxResult.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(350, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormFact
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 607);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBoxResult);
+            this.Controls.Add(this.richTextBoxNum);
+            this.Name = "FormFact";
+            this.Text = "Factorials";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxNum;
+        private System.Windows.Forms.RichTextBox richTextBoxResult;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Factorials/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace Factorials
+{
+    public partial class FormFact : Form
+    {
+        public FormFact()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBoxResult.Text = "";
+
+            if (!String.IsNullOrWhiteSpace(richTextBoxNum.Text))
+            {
+                DateTime StartTime =  DateTime.Now;
+                try
+                {
+                    BigInteger number = BigInteger.Parse(richTextBoxNum.Text);
+
+                    BigInteger Factorial = new System.Numerics.BigInteger();
+                    Factorial = 1;
+
+                    BigInteger i = new System.Numerics.BigInteger();//аккуратнее, в цикле используется ++, если будут ошибки заменить на BigInteger.Add(BigInt,BigInt)
+
+                    for (i = 1; i <= number; i++)
+                    {
+                        Factorial = BigInteger.Multiply(Factorial, i);
+
+                    }
+
+                    DateTime EndTime = DateTime.Now;
+                    richTextBoxResult.Text += "Calculation time without Threading: " + (EndTime - StartTime).ToString("g") + Environment.NewLine;
+                        //Convert.ToString(EndTime - StartTime, "HH:mm:ss.fffff");
+                    richTextBoxResult.Text += Convert.ToString(Factorial);
+                }
+                catch (ArgumentException)
+                {
+                    richTextBoxResult.Text = "Incorrect data entry";
+                
+                }
+
+                catch (FormatException)
+                {
+                    richTextBoxResult.Text = "Incorrect data entry, FormatException";
+                }
+               
+                catch (System.OverflowException)
+                {
+                    richTextBoxResult.Text = "Incorrect data entry, OverflowException";
+                }
+
+
+            }
+        }
+    }
+}
