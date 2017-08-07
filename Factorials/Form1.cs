09a03c7ed532adbfda1f1a8b51e61211b6f4b60a
Archangel <djarchangel3737@gmail.com>
2017-08-07 18:05:41 +0300

    clearing useless comments, code-styling

--- Factorials/Form1.cs
+++ Factorials/Form1.cs
@@ -17,8 +17,6 @@ namespace Factorials
         public FormFact()
         {
             InitializeComponent();
-           
-
         }
 
         public BigInteger number;
@@ -29,18 +27,13 @@ namespace Factorials
             DateTime StartTime = DateTime.Now;
             BigInteger Factorial = new System.Numerics.BigInteger();
             Factorial = 1;
-
             BigInteger i = new System.Numerics.BigInteger();//аккуратнее, в цикле используется ++, если будут ошибки заменить на BigInteger.Add(BigInt,BigInt)
-
             for (i = 1; i <= number; i++)
             {
                 Factorial = BigInteger.Multiply(Factorial, i);
-
             }
-
             DateTime EndTime = DateTime.Now;
             ResultString += "Calculation time with Threading (iteration): " + (EndTime - StartTime).ToString("g") + Environment.NewLine;
-            //Convert.ToString(EndTime - StartTime, "HH:mm:ss.fffff");
             // richTextBoxResult.Text += Convert.ToString(Factorial); //закомментим вывод числа
         }
 
@@ -48,31 +41,22 @@ namespace Factorials
         void FactorialRecursion()
         {
             DateTime StartTimeRecursion = DateTime.Now;
-            
-
             BigInteger FactorialRecursion = new System.Numerics.BigInteger();
-
-
             BigInteger i = new System.Numerics.BigInteger();//аккуратнее, в цикле используется ++, если будут ошибки заменить на BigInteger.Add(BigInt,BigInt)
-
             for (i = 1; i <= number; i++)
             {
                 if (i == 1)
                     FactorialRecursion = 1;
                 else
                     FactorialRecursion *= i;
-
             }
 
             DateTime EndTimeRecursion = DateTime.Now;
             ResultString += "Calculation time with Threading (recursion): " + (EndTimeRecursion - StartTimeRecursion).ToString("g") + Environment.NewLine;
-            //Convert.ToString(EndTime - StartTime, "HH:mm:ss.fffff");
             //richTextBoxResult.Text += Convert.ToString(FactorialRecursion); //закомментим вывод числа
-        
         }
 
 
-        
 
         public void button1_Click(object sender, EventArgs e)
         {
@@ -83,46 +67,31 @@ namespace Factorials
             {
                 number = BigInteger.Parse(richTextBoxNum.Text);
             }
-
             catch (ArgumentException)
             {
                 richTextBoxResult.Text = "Incorrect data entry";
-
             }
-
             catch (FormatException)
             {
                 richTextBoxResult.Text = "Incorrect data entry, FormatException";
             }
-
             catch (System.OverflowException)
             {
                 richTextBoxResult.Text = "Incorrect data entry, OverflowException";
             }
-            
-            
-            
+    
             Thread thread1 = new Thread(FactorialIteration);
             thread1.Start();
             
             Thread thread2 = new Thread(FactorialRecursion);
             thread2.Start();
 
-
             thread1.Join();
             thread2.Join();
 
-            //thread1.Abort();
-            //thread2.Abort();
             DateTime EndTotal = DateTime.Now;
             richTextBoxResult.Text += ResultString + Environment.NewLine;
             richTextBoxResult.Text += "Calculation time Total: " + (EndTotal - StartTotal).ToString("g");
-
-           
-           
         }
-
-        
-
     }
 }
